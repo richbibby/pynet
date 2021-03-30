8456a65e265a634d0bc452ae3b49057af41d2281
Rich Bibby <richbibby@mac.com>
2021-03-30 14:37:59 +0100

    added files

--- generate_devices_module.py
+++ generate_devices_module.py
@@ -1,11 +1,12 @@
 # IMPORT LIBRARIES
-# THE 'create_devices' FUNCTION IS IMPORTED FROM THE 'utils.py' MODULE
+# THE 'create_devices' FUNCTION IS IMPORTED FROM THE 'create_utils.py' MODULE
+# THE ''create_utils.py' MODULE IS PART OF THE 'util' package
 
-from utils import create_devices
+from util.create_utils import create_devices
 from tabulate import tabulate
 
 # MAIN PROGRAM -----------------------------
 if __name__ == '__main__':
 
-    devices = create_devices(num_subnets=2, num_devices=10)
+    devices = create_devices(num_subnets=5, num_devices=4)
     print("\n", tabulate(devices, headers="keys"))
